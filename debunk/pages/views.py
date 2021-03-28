619dc1401e15c63b8851cba546bbe1f857885c61
sk981102 <sk981102@gmail.com>
2021-03-28 09:15:11 -0400

    website done

--- debunk/pages/views.py
+++ debunk/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from survey.models import SurveyCovid,SurveyOther
 
 # Create your views here.
 
@@ -6,4 +7,44 @@ def home_view(request, *args, **kwargs):
     return render(request, "index.html", {})
 
 def analysis_view(request, *args, **kwargs):
-    return render(request, "analysis.html", {})
+    labels = ["Covid", "Others"]
+
+    queryset = SurveyCovid.objects.all()
+    queryset2 = SurveyOther.objects.all()
+    data=[len(queryset),len(queryset2)]
+
+    v1,v2,v3,v4=0,0,0,0
+    for q in queryset:
+        if q.vaccine_type:
+            v1+=1
+        if q.second_shot:
+            v2+=1
+        if q.side_effect:
+            v3+=1
+        if q.children:
+            v4+=1
+    covid_labels=["Different Vaccines","Second Dose","Side Effect", "Children"]
+    covid_data = [v1,v2,v3,v4]
+
+    v1,v2,v3,v4=0,0,0,0
+    for q in queryset2:
+        if q.autism:
+            v1+=1
+        if q.cancer:
+            v2+=1
+        if q.choice:
+            v3+=1
+        if q.gov:
+            v4+=1
+    other_labels=["Cause Autism","Cause Cancer","Just a Choice", "Don't Trust Government"]
+    other_data = [v1,v2,v3,v4]
+    print(covid_data)
+
+    return render(request, "analysis.html", {
+        'labels': labels,
+        'data': data,
+        'covid_labels': covid_labels,
+        'covid_data': covid_data,
+        'other_labels': other_labels,
+        'other_data': other_data,
+    })
